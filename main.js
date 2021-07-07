8bc8600afe52867647767674ad222b651c6c3c8b
luke guardino <lukeguardino@yahoo.ca>
2021-07-07 16:01:37 -0400

    use axios instead of imdb-api

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ const imdb = require('imdb-api')
 var http = require('http')
 var url = require('url')
 var fs = require('fs');
+const axios = require('axios').default;
 
 var form = fs.readFileSync('./site.html');
 
@@ -11,8 +12,13 @@ http.createServer(function (req, res) {
 }).listen(8080)
 
 
-document.getElementById('mDetails').innerHTML = 'ayo';
+getValue('inception')
+// document.getElementById('mDetails').innerHTML = 'ayo';
 
 function getValue(name) {
-    imdb.get({name: name}, {apiKey: '95b1aff'}).then(console.log).catch(console.log);
+    // imdb.get({name: name}, {apiKey: '95b1aff'}).then(console.log).catch(console.log);
+    axios.get("https://www.omdbapi.com/?apikey=95b1aff&t=" + name).then(function (response) {
+        // handle success
+        console.log(response.data['Title']);
+      });
 }
